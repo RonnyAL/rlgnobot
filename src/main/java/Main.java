46d8d4d67feb6c9d83e0707747149b0299924c71
Ronny <ronny_loekken@hotmail.com>
2018-08-29 15:23:21 +0200

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ import org.jsoup.select.Elements;
 import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.core.EmbedBuilder;
 import java.awt.Color;
-import java.util.ArrayList;
+import java.util.ArrayList; //
 
 
 public class Main extends ListenerAdapter {
